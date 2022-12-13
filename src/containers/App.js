944c26cbe2199fdce0a8c2718dd4a437e9f34ddb
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-13 22:39:03 +0100

    connected App with local server. First steps into geolocation and reverse geolocation API

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,15 +10,33 @@ class App extends Component {
   }
 
   componentDidMount() {  
-    setTimeout(() => {
-      fetch("http://dataservice.accuweather.com/locations/v1/search?q=warszawa&apikey=VoGmC6YG0p3lmNnrcIIVfKbZTdc9dUmp")
-      .then(response => response.json()).then(response => fetch(`http://dataservice.accuweather.com/currentconditions/v1/${response[0].Key}?apikey=VoGmC6YG0p3lmNnrcIIVfKbZTdc9dUmp&details=true`)
-        .then(response => response.json())
-        .then(response => {
-          console.log(response[0])
-          this.setState({weatherObject:response[0]})
-        }));
-    }, 4000)
+    const successCallback = position => {
+      console.log(position)
+    }
+
+    const errorCallback = error => {
+      console.log(error)
+    }
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback); // getting lat/lon geo position for initiated app user position. 
+    // tu należy wykonać kolejny kod tzw odwróconej geolokacji z google API.
+    const connectIt = async () => {
+      const response = await fetch('http://localhost:3600/appmount', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+      })
+      const parse = await response.json();
+      console.log(parse);
+    }
+    connectIt();
+    // setTimeout(() => {
+    //   fetch("http://dataservice.accuweather.com/locations/v1/search?q=warszawa&apikey=VoGmC6YG0p3lmNnrcIIVfKbZTdc9dUmp")
+    //   .then(response => response.json()).then(response => fetch(`http://dataservice.accuweather.com/currentconditions/v1/${response[0].Key}?apikey=VoGmC6YG0p3lmNnrcIIVfKbZTdc9dUmp&details=true`)
+    //     .then(response => response.json())
+    //     .then(response => {
+    //       console.log(response[0])
+    //       this.setState({weatherObject:response[0]})
+    //     }));
+    // }, 4000)
   }
 
   render() {
