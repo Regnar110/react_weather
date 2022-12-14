42b8ebe9b462eb8cc064c277afd6912b1a2f91a6
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-14 23:49:27 +0100

    added handlepositionchange function to app component

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,42 +5,38 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
+      currentLocation: [],
       weatherObject:[]
     }
   }
 
+  handlePositionChange = async ({coords}) => {
+    const { latitude, longitude } = coords;
+      const response = await fetch('http://localhost:3600/geolocation', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        body: JSON.stringify({
+          lat: latitude,
+          lon: longitude
+        }),
+        headers: {"Content-Type": "application/json"}
+      })
+      const {city, country} = await response.json();
+      console.log(city, country)
+      this.setState({currentLocation: [{
+        city: city,
+        country: country
+      }]})
+  }
   componentDidMount() {  
-    const successCallback = position => {
-      console.log(position)
-    }
-
     const errorCallback = error => {
       console.log(error)
     }
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback); // getting lat/lon geo position for initiated app user position. 
-    // tu należy wykonać kolejny kod tzw odwróconej geolokacji z google API.
-    const connectIt = async () => {
-      const response = await fetch('http://localhost:3600/appmount', {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-      })
-      const parse = await response.json();
-      console.log(parse);
-    }
-    connectIt();
-    // setTimeout(() => {
-    //   fetch("http://dataservice.accuweather.com/locations/v1/search?q=warszawa&apikey=VoGmC6YG0p3lmNnrcIIVfKbZTdc9dUmp")
-    //   .then(response => response.json()).then(response => fetch(`http://dataservice.accuweather.com/currentconditions/v1/${response[0].Key}?apikey=VoGmC6YG0p3lmNnrcIIVfKbZTdc9dUmp&details=true`)
-    //     .then(response => response.json())
-    //     .then(response => {
-    //       console.log(response[0])
-    //       this.setState({weatherObject:response[0]})
-    //     }));
-    // }, 4000)
+    navigator.geolocation.getCurrentPosition(this.handlePositionChange, errorCallback); // getting lat/lon geo position for initiated app user position. 
   }
 
   render() {
-    return this.state.weatherObject.length === 0 ? <h1>Loading</h1>: <h1>Ready</h1>; // gdy weatherObject jest zapełniony po metodzie fetch wyświetla weather Ready
+    return this.state.currentLocation.length === 0 ? <h1>Loading</h1>: <h1>Ready</h1>; // gdy weatherObject jest zapełniony po metodzie fetch wyświetla weather Ready
   }
 }
 
