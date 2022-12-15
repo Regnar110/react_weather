9a2556d631ae7ff06bc26b2006f655358a3b18e9
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-15 17:44:23 +0100

    changed componentDidMount content

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,37 +6,45 @@ class App extends Component {
     super();
     this.state = {
       currentLocation: [],
-      weatherObject:[]
+      currentWeather: []
     }
   }
 
-  handlePositionChange = async ({coords}) => {
-    const { latitude, longitude } = coords;
-      const response = await fetch('http://localhost:3600/geolocation', {
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        body: JSON.stringify({
-          lat: latitude,
-          lon: longitude
-        }),
-        headers: {"Content-Type": "application/json"}
-      })
-      const {city, country} = await response.json();
-      console.log(city, country)
-      this.setState({currentLocation: [{
-        city: city,
-        country: country
-      }]})
-  }
+
   componentDidMount() {  
+    const initCurrentUserPosition = async ({coords}) => {
+      const { latitude, longitude } = coords;
+        const response = await fetch('http://localhost:3600/initCurrentUserGeoPosition', {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          mode: 'cors', // no-cors, *cors, same-origin
+          body: JSON.stringify({
+            lat: latitude,
+            lon: longitude
+          }),
+          headers: {"Content-Type": "application/json"}
+        })
+        const {city, country, currentWeather} = await response.json();
+        console.log(city, country, currentWeather)
+        this.setState(
+          {
+            currentLocation: [{
+              city: city,
+              country: country,
+            }],
+            currentWeather: currentWeather
+          })
+    }
     const errorCallback = error => {
       console.log(error)
     }
-    navigator.geolocation.getCurrentPosition(this.handlePositionChange, errorCallback); // getting lat/lon geo position for initiated app user position. 
+    navigator.geolocation.getCurrentPosition(initCurrentUserPosition, errorCallback); // getting lat/lon geo position for initiated app user position. 
   }
 
   render() {
-    return this.state.currentLocation.length === 0 ? <h1>Loading</h1>: <h1>Ready</h1>; // gdy weatherObject jest zapełniony po metodzie fetch wyświetla weather Ready
+    return this.state.currentLocation.length === 0  && this.state.currentWeather ?
+      <h1>Loading</h1>
+    : 
+      <h1>Ready</h1>; // gdy weatherObject jest zapełniony po metodzie fetch wyświetla weather Ready
   }
 }
 
