aa626fcfda5b7bc93eba1b623dcd8569a66cbaae
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-15 21:17:05 +0100

    minor changes in app.js

---
+++ src/components/locationSearch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/locationsearch.scss';
+
+const LocationSearch = (props) => {
+    return(
+        <form>
+            <input type='text' placeholder='city name'></input>
+        </form>
+    )
+}
+
+export default LocationSearch;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import LocationSearch from '../components/LocationSearch';
 import '../styles/App.scss';
 
 class App extends Component {
@@ -10,41 +11,44 @@ class App extends Component {
     }
   }
 
+  initCurrentUserPositionLoad = async ({coords}) => {
+    console.log(coords)
+    const { latitude, longitude } = coords;
+      const response = await fetch('http://localhost:3600/initCurrentUserGeoPosition', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        body: JSON.stringify({
+          lat: latitude,
+          lon: longitude
+        }),
+        headers: {"Content-Type": "application/json"}
+      })
+      const {city, country, currentWeather} = await response.json();
+      console.log(city, country, currentWeather)
+      this.setState(
+        {
+          currentLocation: [{
+            city: city,
+            country: country,
+          }],
+          currentWeather: currentWeather
+        })
+  }
 
   componentDidMount() {  
-    const initCurrentUserPosition = async ({coords}) => {
-      const { latitude, longitude } = coords;
-        const response = await fetch('http://localhost:3600/initCurrentUserGeoPosition', {
-          method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          mode: 'cors', // no-cors, *cors, same-origin
-          body: JSON.stringify({
-            lat: latitude,
-            lon: longitude
-          }),
-          headers: {"Content-Type": "application/json"}
-        })
-        const {city, country, currentWeather} = await response.json();
-        console.log(city, country, currentWeather)
-        this.setState(
-          {
-            currentLocation: [{
-              city: city,
-              country: country,
-            }],
-            currentWeather: currentWeather
-          })
-    }
     const errorCallback = error => {
       console.log(error)
     }
-    navigator.geolocation.getCurrentPosition(initCurrentUserPosition, errorCallback); // getting lat/lon geo position for initiated app user position. 
+    navigator.geolocation.getCurrentPosition(this.initCurrentUserPositionLoad, errorCallback); // getting lat/lon geo position for initiated app user position. 
   }
 
   render() {
     return this.state.currentLocation.length === 0  && this.state.currentWeather ?
       <h1>Loading</h1>
     : 
-      <h1>Ready</h1>; // gdy weatherObject jest zapełniony po metodzie fetch wyświetla weather Ready
+    <React.Fragment>
+      <LocationSearch />
+    </React.Fragment>
   }
 }
 
