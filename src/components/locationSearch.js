6d68b367adc62e643981ed8eb9f851b9363811aa
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-19 12:21:20 +0100

    styling navbar, added some functionality, preparing searchfield to work with place autocomplete google API

--- src/components/locationSearch.js
+++ src/components/locationSearch.js
@@ -3,11 +3,24 @@ import '../styles/locationsearch.scss';
 
 const LocationSearch = (props) => {
     return(
-        <div>
-            <input type='text' placeholder='city name' onChange={props.inputChange}></input>
-            <button type='submit' onClick={props.submitLocationSearch}>Submit data</button>
-        </div>
+        <nav className='top-fixed-nav'>
+            <div className='nav'>
+                social
+                social
+                social
+            </div>
+            <div className='searchField'>
+                 <input id='locationSearchBox' list='locations' type='text' placeholder='city name' onChange={props.inputChange}></input>
+                 <datalist className="searchLocationsDatas" id='locations'>
+                    <option>Gdańsk</option>
+                 </datalist>
+                <button id='locationSearchSubmitButton' type='submit' onClick={props.submitLocationSearch}>Search</button>
+            </div>
+        </nav>
+           
     )
 }
 
 export default LocationSearch;
+
+//https://developers.google.com/maps/documentation/javascript/place-autocomplete
