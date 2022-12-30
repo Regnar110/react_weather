7b384a2f454f48e418b148a1df5bdb96251d9d01
Mateusz <66961646+Regnar110@users.noreply.github.com>
2022-12-31 00:27:23 +0100

    changes in card component

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -2,8 +2,6 @@ import React from 'react';
 import '../styles/weathercard.scss'
 import sun from '../assets/sun.png'
 import sunrise from '../assets/wtb-sunrise.png'
-import WeatherOptionDrop from '../components/WeatherOptionDrop';
-import { Chart } from 'react-charts'
 
 const WeatherCard = () => {
     return(
@@ -15,36 +13,39 @@ const WeatherCard = () => {
                         <span className='wtb-date'>00:28</span>
                         <div className='wtb-data wtb-sunrise'><img src={sunrise} alt='sunrise'/>10:00 - 15:00</div>
                     </div>
-                    <div className='weather-card-main-data'>
-                        <div className='day-name-and-date'>
-                            Monday - 29 December
-                        </div>
-                        <div className='weather-temperature-and-icon'>
-                            <img src={sun} alt='weather-icon'/>
-                            <div className='current-temperature'>
-                            -20°C
-                            </div>
-                        </div>
+                    <div className='day-name-and-date'>
+                        <span>Monday</span>
+                        <span>30 December</span>
                     </div>
-                    <div className='weather-additional-data'>
-                        <div className='clouds'>Mostly cloudly</div>
-                        <div className='rest-additional-data'>
-                            <div className='additional-data apparent-temperature'>
-                                <span className='rad-description'>Apparent</span>
-                                <span className='rad-data'>-10°C</span>
+                    <div className='card-datas'>
+                        <div className='weather-card-main-data'>
+                            <div className='weather-temperature-and-icon'>
+                                <img src={sun} alt='weather-icon'/>
+                                <div className='current-temperature'>
+                                    -20°C
+                                </div>
                             </div>
-                            <div className='additional-data pressure'>
-                                <span className='rad-description'>Pressure</span>
-                                <span  className='rad-data'>1000 hPa</span></div>
-                            <div className='additional-data wind'>
-                                <span className='rad-description'>Wind</span>
-                                <span  className='rad-data'>7 km/h</span>
-                            </div>
+                        </div>
+                        <div className='weather-additional-data'>
+                            <div className='clouds'>Mostly cloudly</div>
+                                <div className='rest-additional-data'>
+                                    <div className='additional-data apparent-temperature'>
+                                        <span className='rad-description'>Apparent</span>
+                                        <span className='rad-data'>-10°C</span>
+                                    </div>
+                                    <div className='additional-data pressure'>
+                                        <span className='rad-description'>Pressure</span>
+                                        <span  className='rad-data'>1000 hPa</span>
+                                    </div>
+                                    <div className='additional-data wind'>
+                                        <span className='rad-description'>Wind</span>
+                                        <span  className='rad-data'>7 km/h</span>
+                                    </div>
+                                </div>
+                            </div>    
                         </div>
                     </div>
                 </div>
-                <WeatherOptionDrop />
-            </div>
         </React.Fragment>
     )
 }
